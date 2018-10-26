bd84c657bfe1619bd15753ebf8f2f6baf2842dca
Vishwaram Maharaj <vishwaramm@gmail.com>
2018-10-25 21:21:10 -0400

    remove large unit test

--- file-parser/tests/test_file_parser.py
+++ file-parser/tests/test_file_parser.py
@@ -12,7 +12,6 @@ class TestFileParser(unittest.TestCase):
         self.csv50Filename = self.path / "csv_50.txt"
         self.pipe50Filename = self.path / "pipe_50.txt"
         self.space50Filename = self.path / "space_50.txt"
-        self.largeCsv = self.path / "csv_large.txt"
 
         generator = fg.FileGenerator()
 
@@ -24,9 +23,6 @@ class TestFileParser(unittest.TestCase):
 
         if self.space50Filename.is_file() == False:
             generator.generate(self.space50Filename, 50, " ")
-
-        if self.largeCsv.is_file() == False:
-            generator.generate(self.largeCsv, 10000000, ",")
 
     def test_Parse_Csv(self):
         parser = fp.FileParser()
@@ -48,13 +44,6 @@ class TestFileParser(unittest.TestCase):
 
         self.assertIsNotNone(people)
         self.assertEqual(len(people), 50)
-
-    def test_Parse_LargeFile(self):
-        parser = fp.FileParser()
-        people = parser.parse(self.largeCsv)
-
-        self.assertIsNotNone(people)
-        self.assertEqual(len(people), 10000000)
 
     def test_Parse_Bad_Input_Csv(self):
         parser = fp.FileParser()

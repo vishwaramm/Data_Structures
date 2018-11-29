de18710ce5891ba0a54d01a794c9dbef970ddb05
Vishwaram Maharaj <vishwaramm@gmail.com>
2018-11-28 21:01:35 -0500

    hello doubly linked list, how are you today|?

---
+++ Vish.DataStructures/Vish.DataStructures.Tests/DoublyLinkedListTests.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vish.DataStructures.Models;
+
+namespace Vish.DataStructures.Tests
+{
+    [TestClass]
+    public class DoublyLinkedListTests
+    {
+        [TestMethod]
+        public void Test_IntegerDoublyList_Insert()
+        {
+            DoublyLinkedList<int> list = new DoublyLinkedList<int>();
+            int[] nums = new int[] { 1, 5, 10, 40, 86, 77, 23, 32 };
+
+            foreach (int num in nums)
+            {
+                list.Insert(num);
+            }
+
+            var node = list.GetFirstNode();
+
+            Assert.IsNotNull(node);
+            Assert.AreEqual(node.Value, 1);
+
+            node = list.Search(5);
+
+            Assert.IsNotNull(node);
+            Assert.AreEqual(node.Value, 5);
+
+            node = list.GetLastNode();
+
+            Assert.IsNotNull(node);
+            Assert.AreEqual(node.Value, 32);
+        }
+
+        [TestMethod]
+        public void Test_IntegerDoublyList_Search()
+        {
+            DoublyLinkedList<int> list = new DoublyLinkedList<int>();
+            int[] nums = new int[] { 1, 5, 10, 40, 86, 77, 23, 32 };
+
+            foreach (int num in nums)
+            {
+                list.Insert(num);
+            }
+
+            var node = list.Search(1);
+
+            Assert.IsNotNull(node);
+            Assert.AreEqual(node.Value, 1);
+
+            node = list.Search(5);
+
+            Assert.IsNotNull(node);
+            Assert.AreEqual(node.Value, 5);
+
+            node = list.Search(32);
+
+            Assert.IsNotNull(node);
+            Assert.AreEqual(node.Value, 32);
+
+            node = list.Search(322);
+
+            Assert.IsNull(node);
+        }
+
+        [TestMethod]
+        public void Test_IntegerDoublyList_Delete()
+        {
+            DoublyLinkedList<int> list = new DoublyLinkedList<int>();
+            int[] nums = new int[] { 1, 5, 10, 40, 86, 77, 23, 32 };
+
+            foreach (int num in nums)
+            {
+                list.Insert(num);
+            }
+
+            var node = list.Search(1);
+
+            list.Delete(node);
+            node = list.Search(1);
+            Assert.IsNull(node);
+
+            node = list.Search(32);
+
+            list.Delete(node);
+            node = list.Search(32);
+            Assert.IsNull(node);
+
+            node = list.Search(86);
+
+            list.Delete(node);
+            node = list.Search(86);
+            Assert.IsNull(node);
+            
+        }
+    }
+}
---
+++ Vish.DataStructures/Vish.DataStructures/Models/DoublyLinkedList.cs
@@ -0,0 +1,123 @@
+﻿using System;
+namespace Vish.DataStructures.Models
+{
+    public class DoublyLinkedList<T>
+    {
+        private DoublyLinkedListNode<T> Head { get; set; }
+        private DoublyLinkedListNode<T> Tail { get; set; }
+
+        public DoublyLinkedList() {}
+
+        public DoublyLinkedList(DoublyLinkedListNode<T> head, DoublyLinkedListNode<T> tail)
+        {
+            Head = head;
+            Tail = tail;
+        }
+
+        //Runtime O(1)
+        public DoublyLinkedListNode<T> GetFirstNode()
+        {
+            return Head;
+        }
+
+        //Runtime O(1)
+        public DoublyLinkedListNode<T> GetLastNode()
+        {
+            if (Tail == null)
+            {
+                return Head;
+            }
+            else
+            {
+                return Tail;
+            }
+        }
+
+        //Runtime O(n)
+        public void Insert(T val)
+        {
+            if (Head == null)
+            {
+                Head = new DoublyLinkedListNode<T>(val);
+            }
+            else
+            {
+                var node = new DoublyLinkedListNode<T>(val);
+                if (Tail == null) //this will only be null if there's one element in the list
+                {
+                    Tail = node;
+                    Tail.Previous = Head; //set head as prev
+                    Head.Next = Tail;
+                }
+                else
+                {
+                    Tail.Next = node;
+                    node.Previous = Tail; //set old tail as previous to new tail
+                    Tail = node; //set the new tail
+                }
+            }
+        }
+
+        //Runtime O(n)
+        public void Delete(DoublyLinkedListNode<T> node)
+        {
+            var current = Head;
+
+            while (current != node)
+            {
+                current = current.Next;
+            }
+
+            if (current != null) //found it
+            {
+                if (current == Head)
+                {
+                    Head = current.Next;
+                }
+                else if (current == Tail)
+                {
+                    Tail = current.Previous;
+                }
+
+                if (current.Previous != null)
+                {
+                    current.Previous.Next = current.Next;
+                }
+
+                if (current.Next != null)
+                {
+                    current.Next.Previous = current.Previous;
+                }
+            }
+        }
+
+        public DoublyLinkedListNode<T> Search(T val)
+        {
+            var current = Head;
+
+            while (current != null)
+            {
+                if (current.Value.Equals(val))
+                {
+                    return current;
+                }
+
+                current = current.Next;
+            }
+
+            return null;
+        }
+    }
+
+    public class DoublyLinkedListNode<T>
+    {
+        public T Value { get; set; }
+        public DoublyLinkedListNode<T> Next { get; set; }
+        public DoublyLinkedListNode<T> Previous { get; set; }
+
+        public DoublyLinkedListNode(T val)
+        {
+            Value = val;
+        }
+    }
+}

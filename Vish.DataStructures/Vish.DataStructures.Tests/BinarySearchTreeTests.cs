7afe2e0ae959cec7bf791dea5631a22101107673
Vishwaram Maharaj <vishwaramm@gmail.com>
2018-12-03 20:06:59 -0500

    binary search tree

---
+++ Vish.DataStructures/Vish.DataStructures.Tests/BinarySearchTreeTests.cs
@@ -0,0 +1,368 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vish.DataStructures.Models;
+
+namespace Vish.DataStructures.Tests
+{
+    [TestClass]
+    public class BinarySearchTreeTests
+    {
+        [TestMethod]
+        public void Test_BST_Insert()
+        {
+            BinaryTreeNode<int> root = new BinaryTreeNode<int>(5);
+
+            root.Insert(4);
+
+            Assert.AreEqual(root.Left.Value, 4);
+
+            root.Insert(10);
+
+            Assert.AreEqual(root.Right.Value, 10);
+
+            root.Insert(2);
+
+            Assert.AreEqual(root.Left.Left.Value, 2);
+
+            root.Insert(8);
+
+            Assert.AreEqual(root.Right.Left.Value, 8);
+
+            root.Insert(14);
+
+            Assert.AreEqual(root.Right.Right.Value, 14);
+        }
+
+        [TestMethod]
+        public void Test_BST_DeleteLeftNodeWithChildren()
+        {
+            BinaryTreeNode<int> root = new BinaryTreeNode<int>(10);
+
+            root.Insert(9);
+
+            root.Insert(8);
+
+            root.Insert(11);
+
+            root.Insert(5);
+
+            root.Insert(7);
+
+            root.Insert(23);
+
+            root.Insert(4);
+
+            var node = root.Search(9);
+
+            root.Delete(node);
+
+            Assert.AreEqual(8, root.Left.Value);
+            Assert.AreEqual(7, root.Left.Left.Value);
+            Assert.AreEqual(5, root.Left.Left.Left.Value);
+            Assert.AreEqual(4, root.Left.Left.Left.Left.Value);
+            Assert.IsNull(root.Left.Left.Left.Right);
+        }
+
+        [TestMethod]
+        public void Test_BST_DeleteRightNodeWithChildren()
+        {
+            BinaryTreeNode<int> root = new BinaryTreeNode<int>(10);
+
+            /*
+             * 
+             *                10
+             *              /   \
+                           9     11
+                          /       \
+                         8         23
+                        /          / \
+                       5          12  36
+                        \          \
+                         7          14
+             */           
+
+            root.Insert(9);
+
+            root.Insert(8);
+
+            root.Insert(11);
+
+            root.Insert(5);
+
+            root.Insert(7);
+
+            root.Insert(23);
+
+            root.Insert(4);
+
+            root.Insert(12);
+
+            root.Insert(14);
+
+            root.Insert(36);
+
+            var node = root.Search(23);
+
+            root.Delete(node);
+
+            Assert.AreEqual(14, root.Right.Right.Value);
+            Assert.AreEqual(12, root.Right.Right.Left.Value);
+            Assert.AreEqual(36, root.Right.Right.Right.Value);
+            Assert.IsNull(root.Right.Right.Left.Right);
+        }
+
+        [TestMethod]
+        public void Test_BST_Search()
+        {
+            BinaryTreeNode<int> root = new BinaryTreeNode<int>(5);
+
+            root.Insert(2);
+            root.Insert(3);
+            root.Insert(10);
+            root.Insert(80);
+            root.Insert(23);
+            root.Insert(100);
+            root.Insert(9);
+            root.Insert(8);
+            root.Insert(11);
+            root.Insert(13);
+            root.Insert(22);
+
+            var node = root.Search(22);
+
+            Assert.IsNotNull(node);
+            Assert.AreEqual(22, node.Value);
+
+            var empty = root.Search(4000000);
+
+            Assert.IsNull(empty);
+        }
+
+        [TestMethod]
+        public void Test_BST_InOrderTraversal()
+        {
+            BinaryTreeNode<int> root = new BinaryTreeNode<int>(10);
+
+            /*
+             * 
+             *                10
+             *              /   \
+                           9     11
+                          /       \
+                         8         23
+                        /          / \
+                       5          12  36
+                        \          \
+                         7          14
+             */
+
+            root.Insert(9);
+
+            root.Insert(8);
+
+            root.Insert(11);
+
+            root.Insert(5);
+
+            root.Insert(7);
+
+            root.Insert(23);
+
+            root.Insert(4);
+
+            root.Insert(12);
+
+            root.Insert(14);
+
+            root.Insert(36);
+
+            var list = root.InOrderTraversal();
+            int[] nums = new int[11] { 4, 5, 7, 8, 9, 10, 11, 12, 14, 23, 36 };
+
+            Assert.AreEqual(list.Count, nums.Length);
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Assert.AreEqual(nums[i], list[i]);
+            }
+        }
+
+        [TestMethod]
+        public void Test_BST_PreOrderTraversal()
+        {
+            BinaryTreeNode<int> root = new BinaryTreeNode<int>(10);
+
+            /*
+             * 
+             *                10
+             *              /   \
+                           9     11
+                          /       \
+                         8         23
+                        /          / \
+                       5          12  36
+                      / \          \
+                     4   7          14
+             */
+
+            root.Insert(9);
+
+            root.Insert(8);
+
+            root.Insert(11);
+
+            root.Insert(5);
+
+            root.Insert(7);
+
+            root.Insert(23);
+
+            root.Insert(4);
+
+            root.Insert(12);
+
+            root.Insert(14);
+
+            root.Insert(36);
+
+            var list = root.PreOrderTraversal();
+            int[] nums = new int[11] { 10, 9, 8, 5, 4, 7, 11, 23, 12, 14, 36 };
+
+            Assert.AreEqual(list.Count, nums.Length);
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Assert.AreEqual(nums[i], list[i]);
+            }
+        }
+
+        [TestMethod]
+        public void Test_BST_PostOrderTraversal()
+        {
+            BinaryTreeNode<int> root = new BinaryTreeNode<int>(10);
+
+            /*
+             * 
+             *                10
+             *              /   \
+                           9     11
+                          /       \
+                         8         23
+                        /          / \
+                       5          12  36
+                      / \          \
+                     4   7          14
+             */
+
+            root.Insert(9);
+
+            root.Insert(8);
+
+            root.Insert(11);
+
+            root.Insert(5);
+
+            root.Insert(7);
+
+            root.Insert(23);
+
+            root.Insert(4);
+
+            root.Insert(12);
+
+            root.Insert(14);
+
+            root.Insert(36);
+
+            var list = root.PostOrderTraversal();
+            int[] nums = new int[11] { 4,7,5,8,9,14,12,36,23,11,10 };
+
+            Assert.AreEqual(list.Count, nums.Length);
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Assert.AreEqual(nums[i], list[i]);
+            }
+        }
+
+        [TestMethod]
+        public void Test_BST_HeightLongerOnLeft()
+        {
+            BinaryTreeNode<int> root = new BinaryTreeNode<int>(5);
+
+            root.Insert(2);
+            root.Insert(8);
+            root.Insert(1);
+            root.Insert(4);
+            root.Insert(3);
+            root.Insert(9);
+
+            int height = root.Height();
+
+            Assert.AreEqual(3, height);
+        }
+
+        [TestMethod]
+        public void Test_BST_HeightLongerOnRight()
+        {
+            BinaryTreeNode<int> root = new BinaryTreeNode<int>(5);
+
+            root.Insert(2);
+            root.Insert(8);
+            root.Insert(1);
+            root.Insert(9);
+            root.Insert(6);
+            root.Insert(7);
+            root.Insert(10);
+
+            int height = root.Height();
+
+            Assert.AreEqual(3, height);
+        }
+
+        [TestMethod]
+        public void Test_BST_HeightSameOnBothSides()
+        {
+            BinaryTreeNode<int> root = new BinaryTreeNode<int>(5);
+
+            root.Insert(2);
+            root.Insert(8);
+            root.Insert(1);
+            root.Insert(4);
+            root.Insert(3);
+            root.Insert(9);
+            root.Insert(6);
+            root.Insert(7);
+
+            int height = root.Height();
+
+            Assert.AreEqual(3, height);
+        }
+
+        [TestMethod]
+        public void Test_BST_Leaf()
+        {
+            BinaryTreeNode<int> root = new BinaryTreeNode<int>(5);
+
+            Assert.IsTrue(root.IsLeaf());
+
+            root.Insert(10);
+
+            Assert.IsFalse(root.IsLeaf());
+
+            root.Insert(3);
+
+            Assert.IsFalse(root.IsLeaf());
+
+            root.Insert(2);
+
+            var node = root.Search(3);
+
+            Assert.IsFalse(node.IsLeaf());
+
+            node = root.Search(2);
+
+            Assert.IsTrue(node.IsLeaf());
+        }
+    }
+}
---
+++ Vish.DataStructures/Vish.DataStructures/Models/BinarySearchTree.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Vish.DataStructures.Models
+{
+    public class BinaryTreeNode<T> where T : IComparable
+    {
+        public T Value { get; set; }
+
+        public BinaryTreeNode<T> Left { get; set; }
+
+        public BinaryTreeNode<T> Right { get; set; }
+
+        public BinaryTreeNode(T val)
+        {
+            Value = val;
+        }
+
+        /// <summary>
+        /// Whether this node has any children or is the last node in its branch. Runtime O(1)
+        /// </summary>
+        /// <returns><c>true</c>, if leaf was ised, <c>false</c> otherwise.</returns>
+        public bool IsLeaf()
+        {
+            return Left == null && Right == null;
+        }
+
+        /// <summary>
+        /// Get height of tree. Runtime O(n)
+        /// </summary>
+        /// <returns>The height.</returns>
+        public int Height()
+        {
+            return zHeight(this);
+        }
+
+
+        private int zHeight(BinaryTreeNode<T> node)
+        {
+            if (node == null)
+                return -1;
+
+            return 1 + Math.Max(zHeight(node.Left), zHeight(node.Right));
+        }
+
+        /// <summary>
+        /// Inserts the specified value in the appropriate location in the tree. Runtime O(Log n)
+        /// </summary>
+        /// <param name="val">Value.</param>
+        public void Insert(T val)
+        {
+            if (val.CompareTo(Value) > 0)
+            {
+                if (Right == null)
+                    Right = new BinaryTreeNode<T>(val);
+                else
+                    Right.Insert(val);
+            }
+            else
+            {
+                if (Left == null)
+                    Left = new BinaryTreeNode<T>(val);
+                else
+                    Left.Insert(val);
+            }
+        }
+
+        /// <summary>
+        /// Deletes the specified node. Runtime O(Log n)
+        /// </summary>
+        /// <param name="node">Node.</param>
+        public void Delete(BinaryTreeNode<T> node)
+        {
+            if (node.Value.CompareTo(Value) > 0)
+            {
+                if (Right == node)
+                {
+                    if (Right.Left != null)
+                    {
+                        var rightMost = zGetRightMostNode(Right.Left);
+                        Right.Value = rightMost.Value; //replaced with right most node because the right most is the largest value in the left subtree
+                        Delete(rightMost);
+                    }
+                    else if (Right.Right != null)
+                    {
+                        Right.Value = Right.Right.Value;
+                        Delete(Right.Right);
+                    }
+                    else
+                    {
+                        Right = null;
+                    }
+                }
+                else
+                {
+                    Right.Delete(node);
+                }
+            }
+            else
+            {
+                if (Left == node)
+                {
+                    if (Left.Left != null)
+                    {
+                        var rightMost = zGetRightMostNode(Left.Left);
+                        Left.Value = rightMost.Value;
+                        Delete(rightMost);
+                    }
+                    else if (Left.Right != null)
+                    {
+                        Left.Value = Left.Right.Value;
+                        Delete(Left.Right);
+                    }
+                    else
+                    {
+                        Left = null;
+                    }
+                }
+                else
+                {
+                    Left.Delete(node);
+                }
+            }
+        }
+
+        private BinaryTreeNode<T> zGetRightMostNode(BinaryTreeNode<T> node)
+        {
+            var current = node;
+            BinaryTreeNode<T> result = current;
+
+            while (true)
+            {
+                current = current.Right;
+
+                if (current == null)
+                    break;
+
+                result = current;
+            }
+
+            return result;
+        }
+
+        public BinaryTreeNode<T> Search(T val)
+        {
+            if (Value.Equals(val))
+            {
+                return this;
+            }
+            else if (val.CompareTo(Value) > 0)
+            {
+                if (Right != null)
+                {
+                    return Right.Search(val);
+                }
+                return null;
+            }
+            else
+            {
+                if (Left != null)
+                {
+                    return Left.Search(val);
+                }
+
+                return null;
+            }
+        }
+
+        public List<T> InOrderTraversal()
+        {
+            List<T> result = new List<T>();
+
+            zInOrderTraversal(this, result);
+
+            return result;
+        }
+
+        private void zInOrderTraversal(BinaryTreeNode<T> node, List<T> list)
+        {
+            if (node == null)
+                return;
+
+            zInOrderTraversal(node.Left, list);
+
+            list.Add(node.Value);
+
+            zInOrderTraversal(node.Right, list);
+        }
+
+        public List<T> PreOrderTraversal()
+        {
+            List<T> result = new List<T>();
+            zPreOrderTraversal(this, result);
+
+            return result;
+        }
+
+        private void zPreOrderTraversal(BinaryTreeNode<T> node, List<T> list)
+        {
+            if (node == null)
+                return;
+
+            list.Add(node.Value);
+            zPreOrderTraversal(node.Left, list);
+            zPreOrderTraversal(node.Right, list);
+        }
+
+        public List<T> PostOrderTraversal()
+        {
+            List<T> result = new List<T>();
+            zPostOrderTraversal(this, result);
+
+            return result;
+        }
+
+        private void zPostOrderTraversal(BinaryTreeNode<T> node, List<T> list)
+        {
+            if (node == null)
+                return;
+
+            zPostOrderTraversal(node.Left, list);
+            zPostOrderTraversal(node.Right, list);
+            list.Add(node.Value);
+        }
+    }
+}

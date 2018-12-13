d1bccd1d0645e5fa4e3b25b4294465bb2ae8e4f5
Vishwaram Maharaj <vishwaramm@gmail.com>
2018-12-13 12:35:08 -0500

    clean up repo

--- game-of-war/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.css';
-import War from './components/War';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<War />, document.getElementById('root'));
-registerServiceWorker();
--- game-of-war/src/tests/War.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import War from './components/War';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<War />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
